5ba13a1ad0c0a15855a0b780265bbbc264e6c661
Gaël Foppolo <me@gaelfoppolo.com>
2015-03-10 07:59:44 +0000

    premier import

---
+++ HandiParking/HandiParking/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HandiParking
+//
+//  Created by Gaël on 10/03/2015.
+//  Copyright (c) 2015 KeepCore. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
